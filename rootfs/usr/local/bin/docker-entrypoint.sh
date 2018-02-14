6d7e0248d4396aaed2a463fc00ab464170924de0
Ernesto Pérez <westerus@gmail.com>
2018-02-15 00:45:01 +0100

    Fix variable VERSION in docker-entrypoint

--- rootfs/usr/local/bin/docker-entrypoint.sh
+++ rootfs/usr/local/bin/docker-entrypoint.sh
@@ -5,7 +5,7 @@ shopt -s nullglob
 if [ $1 = "webui" ]; then
   DIRPATH=/srv/aptly/public
   URL=https://github.com/sdumetz/aptly-web-ui/releases
-  VERSION=LATEST_RELEASE=$(curl -L -s -H 'Accept: application/json' $URL/latest|sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/')
+  VERSION=$(curl -L -s -H 'Accept: application/json' $URL/latest|sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/')
   if [ ! -d "$DIRPATH" ]; then
     echo "======= DEPLOY WEB INTERFACE ======="
     mkdir -p $DIRPATH
