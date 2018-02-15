67ddc74744e96f136b0ddbbb785893ff686723e5
Ernesto Pérez <westerus@gmail.com>
2018-02-15 01:11:40 +0100

    Update docker-entrypoint

--- rootfs/usr/local/bin/docker-entrypoint.sh
+++ rootfs/usr/local/bin/docker-entrypoint.sh
@@ -2,14 +2,18 @@
 set -eo pipefail
 shopt -s nullglob
 
-if [ $1 = "webui" ]; then
+if [ $WEBUI = "yes" ]; then
   DIRPATH=/srv/aptly/public
   URL=https://github.com/sdumetz/aptly-web-ui/releases
   VERSION=$(curl -L -s -H 'Accept: application/json' $URL/latest|sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/')
-  if [ ! -d "$DIRPATH" ]; then
+  if [ ! -d "$DIRPATH/ui" ]; then
     echo "======= DEPLOY WEB INTERFACE ======="
     mkdir -p $DIRPATH
     curl -SL $URL/download/$VERSION/aptly-web-ui.tar.gz |tar xzv -C $DIRPATH
+    echo "=========== FINISH DEPLOY =========="
+  else
+    echo "========== !!CANCEL DEPLOY ============"
+    echo "======= ALREADY DEPLOYED WEBUI ======="
   fi
 fi
 
